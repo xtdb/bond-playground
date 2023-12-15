e557c99b94afc8cc8d9bcef5c46f98b20d0fd568
James Henderson <james@jarohen.dev>
2023-12-15 10:07:27 +0000

    no longer labs

--- src/user.clj
+++ src/user.clj
@@ -6,9 +6,9 @@
 (def bond-tx-ops
   (vec
    (for [doc (read-string (slurp (io/resource "james-bond.edn")))]
-     [:put (:type doc)
-      (-> doc
-          (dissoc :type))])))
+     (xt/put (:type doc)
+             (-> doc
+                 (dissoc :type))))))
 
 (def xt-node
   (let [node (xtn/start-node {})]
@@ -19,6 +19,17 @@
   ;; `SELECT xt$id, person$name FROM person WHERE xt$id = 'daniel-craig'`
   (xt/q xt-node '(from :person [{:xt/id :daniel-craig}
                                 xt/id person/name]))
+
+  (xt/q xt-node '(-> (unify (from :film [{:film/bond :daniel-craig}
+                                         film/bond film/name])
+                            (from :person [{:xt/id film/bond} person/name]))
+                     (without :film/bond)))
+
+  (xt/q xt-node '(-> (unify (from :film [{:film/bond-girls bond-girls} film/name])
+                            (unnest {bond-girl bond-girls})
+                            (from :person [{:xt/id bond-girl} person/name]))
+                     (return film/name person/name)
+                     (limit 2)))
 
   ;; Have fun!
   )
